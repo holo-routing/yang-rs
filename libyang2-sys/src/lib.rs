b5cb798142de54e3dc2f7e31a6c1c72903faf3ea
Wataru Ishida <wataru.ishid@gmail.com>
2021-11-30 06:24:47 +0000

    Suppress UB warnings from bindgen
    See https://github.com/rust-lang/rust-bindgen/issues/1651
    
    Signed-off-by: Wataru Ishida <wataru.ishid@gmail.com>

--- libyang2-sys/src/lib.rs
+++ libyang2-sys/src/lib.rs
@@ -1,9 +1,13 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-// Silence "128-bit integers don't currently have a known stable ABI" warnings
-#![allow(improper_ctypes)]
-// Silence "constants have by default a `'static` lifetime" clippy warnings
-#![allow(clippy::redundant_static_lifetimes)]
+#![allow(
+    non_upper_case_globals,
+    non_camel_case_types,
+    non_snake_case,
+    // Silence "128-bit integers don't currently have a known stable ABI" warnings
+    improper_ctypes,
+    // Silence "constants have by default a `'static` lifetime" clippy warnings
+    clippy::redundant_static_lifetimes,
+    // https://github.com/rust-lang/rust-bindgen/issues/1651
+    deref_nullptr,
+)]
 
 include!(concat!(env!("OUT_DIR"), "/libyang2.rs"));

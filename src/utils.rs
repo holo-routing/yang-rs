cdefbba0c5e948e6455e59a0a704b78cce3f8d7a
Renato Westphal <renato@opensourcerouting.org>
2024-07-13 15:29:56 -0300

    Fix inconsistent doc comments
    Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

--- src/utils.rs
+++ src/utils.rs
@@ -7,12 +7,12 @@
 use std::ffi::CStr;
 use std::os::raw::c_char;
 
-/// Convert C String to string slice.
+/// Convert C String to owned string.
 pub(crate) fn char_ptr_to_string(c_str: *const c_char) -> String {
     unsafe { CStr::from_ptr(c_str).to_string_lossy().into_owned() }
 }
 
-/// Convert C String to optional string slice.
+/// Convert C String to optional owned string.
 pub(crate) fn char_ptr_to_opt_string(c_str: *const c_char) -> Option<String> {
     if c_str.is_null() {
         None
@@ -21,12 +21,12 @@ pub(crate) fn char_ptr_to_opt_string(c_str: *const c_char) -> Option<String> {
     }
 }
 
-/// Convert C String to owned string.
+/// Convert C String to string slice.
 pub(crate) fn char_ptr_to_str<'a>(c_str: *const c_char) -> &'a str {
     unsafe { CStr::from_ptr(c_str).to_str().unwrap() }
 }
 
-/// Convert C String to optional owned string.
+/// Convert C String to optional string slice.
 pub(crate) fn char_ptr_to_opt_str<'a>(c_str: *const c_char) -> Option<&'a str> {
     if c_str.is_null() {
         None

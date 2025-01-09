holo-routing/yang-rs

commit b6e8c6b585cd5658266248f83a5889f16a1bc72c

Fix two instances of non-null-terminated strings in FFI calls
Regression introduced by commit a2eb883b6c4f.

Reported-by: Christian Hopps <chopps@labn.net>
Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

src/data.rs
@@ -496,11 +496,12 @@ impl<'a> DataTree<'a> {
     ) -> Result<DataTree<'a>> {
         let mut rnode = std::ptr::null_mut();
         let rnode_ptr = &mut rnode;
+        let cdata = CString::new(data.as_ref()).unwrap();
 
         let ret = unsafe {
             ffi::lyd_parse_data_mem(
                 context.raw,
-                data.as_ref().as_ptr() as _,
+                cdata.as_ptr() as _,
                 format as u32,
                 parser_options.bits(),
                 validation_options.bits(),
@@ -525,10 +526,10 @@ impl<'a> DataTree<'a> {
         let rnode_ptr = &mut rnode;
 
         // Create input handler.
+        let cdata = CString::new(data.as_ref()).unwrap();
         let mut ly_in = std::ptr::null_mut();
-        let ret = unsafe {
-            ffi::ly_in_new_memory(data.as_ref().as_ptr() as _, &mut ly_in)
-        };
+        let ret =
+            unsafe { ffi::ly_in_new_memory(cdata.as_ptr() as _, &mut ly_in) };
         if ret != ffi::LY_ERR::LY_SUCCESS {
             return Err(Error::new(context));
         }

commit bd2c59c19ce022765cad2825ac0e06fffbcc8e3c

Fix parsing of LYB instance data
libyang expects JSON and XML instance data to be null-terminated,
but LYB instance data isn't null-terminated and may contain null
bytes within the data. This fix ensures proper handling by treating
these cases separately.

Regression introduced by commit b6e8c6b585c.

Signed-off-by: Renato Westphal <renato@opensourcerouting.org>

src/data.rs
@@ -514,10 +514,17 @@ impl<'a> DataTree<'a> {
         };
 
         // Create input handler.
-        let cdata = CString::new(data.as_ref()).unwrap();
+        let cdata;
         let mut ly_in = std::ptr::null_mut();
-        let ret =
-            unsafe { ffi::ly_in_new_memory(cdata.as_ptr() as _, &mut ly_in) };
+        let ret = match format {
+            DataFormat::XML | DataFormat::JSON => unsafe {
+                cdata = CString::new(data.as_ref()).unwrap();
+                ffi::ly_in_new_memory(cdata.as_ptr() as _, &mut ly_in)
+            },
+            DataFormat::LYB => unsafe {
+                ffi::ly_in_new_memory(data.as_ref().as_ptr() as _, &mut ly_in)
+            },
+        };
         if ret != ffi::LY_ERR::LY_SUCCESS {
             return Err(Error::new(context));
         }
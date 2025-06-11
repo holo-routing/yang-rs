757547f60552c79e67c31a34c512869b3b8ee13b
Ahmed Elhassany <Ahmed.Elhassany@swisscom.com>
2025-06-11 22:46:48 +0200

    Rework the bundled feature flag
    yang-rs with bundled feature is building a shared libyang object, not
    a static one as expected by this feature. This commit reworks the
    implementation of bundled flag to use only cmake for the build
    with the correct flags to generate a static object `.a`. Also,
    remove the manual build steps using `cc`.
    The bindgen feature now will regenerate bindings based on the
    newly compiled code if the bundled feature is enabled.

--- libyang3-sys/build.rs
+++ libyang3-sys/build.rs
@@ -6,6 +6,7 @@ fn main() {
     let out_file = dst.join("libyang3.rs");
 
     #[cfg(feature = "bindgen")]
+    #[cfg(not(feature = "bundled"))]
     {
         // Generate Rust FFI to libyang.
         println!("cargo:rerun-if-changed=wrapper.h");
@@ -35,109 +36,49 @@ fn main() {
     {
         use std::path::Path;
         use std::process::Command;
-
         // Initialize the libyang submodule if necessary.
         if !Path::new("libyang/.git").exists() {
             let _ = Command::new("git")
                 .args(&["submodule", "update", "--init"])
                 .status();
         }
-
-        // Run cmake.
-        let cmake_dst = cmake::build("libyang");
-
-        // Build libyang3.
-        let mut build = cc::Build::new();
-        build
-            .include(format!("{}/build/compat", cmake_dst.display()))
-            .include(format!("{}/build/src", cmake_dst.display()))
-            .include(format!("{}/build/libyang", cmake_dst.display()))
-            .include("libyang/src")
-            .include("libyang/src/plugins_exts")
-            .file("libyang/compat/compat.c")
-            .file("libyang/src/context.c")
-            .file("libyang/src/dict.c")
-            .file("libyang/src/diff.c")
-            .file("libyang/src/hash_table.c")
-            .file("libyang/src/in.c")
-            .file("libyang/src/json.c")
-            .file("libyang/src/log.c")
-            .file("libyang/src/lyb.c")
-            .file("libyang/src/ly_common.c")
-            .file("libyang/src/out.c")
-            .file("libyang/src/parser_common.c")
-            .file("libyang/src/parser_json.c")
-            .file("libyang/src/parser_lyb.c")
-            .file("libyang/src/parser_xml.c")
-            .file("libyang/src/parser_yang.c")
-            .file("libyang/src/parser_yin.c")
-            .file("libyang/src/path.c")
-            .file("libyang/src/plugins.c")
-            .file("libyang/src/plugins_exts.c")
-            .file("libyang/src/plugins_exts/metadata.c")
-            .file("libyang/src/plugins_exts/nacm.c")
-            .file("libyang/src/plugins_exts/schema_mount.c")
-            .file("libyang/src/plugins_exts/structure.c")
-            .file("libyang/src/plugins_exts/yangdata.c")
-            .file("libyang/src/plugins_types.c")
-            .file("libyang/src/plugins_types/binary.c")
-            .file("libyang/src/plugins_types/bits.c")
-            .file("libyang/src/plugins_types/boolean.c")
-            .file("libyang/src/plugins_types/date_and_time.c")
-            .file("libyang/src/plugins_types/decimal64.c")
-            .file("libyang/src/plugins_types/empty.c")
-            .file("libyang/src/plugins_types/enumeration.c")
-            .file("libyang/src/plugins_types/hex_string.c")
-            .file("libyang/src/plugins_types/identityref.c")
-            .file("libyang/src/plugins_types/instanceid.c")
-            .file("libyang/src/plugins_types/instanceid_keys.c")
-            .file("libyang/src/plugins_types/integer.c")
-            .file("libyang/src/plugins_types/ipv4_address.c")
-            .file("libyang/src/plugins_types/ipv4_address_no_zone.c")
-            .file("libyang/src/plugins_types/ipv4_prefix.c")
-            .file("libyang/src/plugins_types/ipv6_address.c")
-            .file("libyang/src/plugins_types/ipv6_address_no_zone.c")
-            .file("libyang/src/plugins_types/ipv6_prefix.c")
-            .file("libyang/src/plugins_types/leafref.c")
-            .file("libyang/src/plugins_types/lyds_tree.c")
-            .file("libyang/src/plugins_types/node_instanceid.c")
-            .file("libyang/src/plugins_types/time_period.c")
-            .file("libyang/src/plugins_types/string.c")
-            .file("libyang/src/plugins_types/union.c")
-            .file("libyang/src/plugins_types/xpath1.0.c")
-            .file("libyang/src/printer_data.c")
-            .file("libyang/src/printer_json.c")
-            .file("libyang/src/printer_lyb.c")
-            .file("libyang/src/printer_schema.c")
-            .file("libyang/src/printer_tree.c")
-            .file("libyang/src/printer_xml.c")
-            .file("libyang/src/printer_yang.c")
-            .file("libyang/src/printer_yin.c")
-            .file("libyang/src/schema_compile_amend.c")
-            .file("libyang/src/schema_compile.c")
-            .file("libyang/src/schema_compile_node.c")
-            .file("libyang/src/schema_features.c")
-            .file("libyang/src/set.c")
-            .file("libyang/src/tree_data.c")
-            .file("libyang/src/tree_data_common.c")
-            .file("libyang/src/tree_data_free.c")
-            .file("libyang/src/tree_data_hash.c")
-            .file("libyang/src/tree_data_new.c")
-            .file("libyang/src/tree_data_sorted.c")
-            .file("libyang/src/tree_schema.c")
-            .file("libyang/src/tree_schema_common.c")
-            .file("libyang/src/tree_schema_free.c")
-            .file("libyang/src/validation.c")
-            .file("libyang/src/xml.c")
-            .file("libyang/src/xpath.c")
-            .warnings(false);
-
-        build.compile("yang3");
+        // Run cmake configure and build libyang
+        let mut cmake_config = cmake::Config::new("libyang");
+        cmake_config.define("BUILD_SHARED_LIBS", "OFF"); // Force static linking
+        cmake_config.define("ENABLE_TESTS", "OFF");
+        cmake_config.define("ENABLE_VALGRIND_TESTS", "OFF");
+        cmake_config.define("ENABLE_BUILD_TESTS", "OFF");
+        cmake_config.define("CMAKE_BUILD_TYPE", "Release");
+        cmake_config.define("CMAKE_POSITION_INDEPENDENT_CODE", "ON");
+        let cmake_dst = cmake_config.build();
         println!("cargo:root={}", env::var("OUT_DIR").unwrap());
+        println!("cargo:rustc-link-search=native={}/lib", cmake_dst.display());
+        println!(
+            "cargo:rustc-link-search=native={}/lib64",
+            cmake_dst.display()
+        );
         if let Err(e) = pkg_config::Config::new().probe("libpcre2-8") {
             println!("cargo:warning=failed to find pcre2 library with pkg-config: {}", e);
             println!("cargo:warning=attempting to link without pkg-config");
             println!("cargo:rustc-link-lib=pcre2-8");
+        }
+        println!("cargo:rustc-link-lib=static=yang");
+        println!("cargo:rerun-if-changed=libyang");
+
+        #[cfg(feature = "bindgen")]
+        {
+            // Use the newly compiled libyang code to generate Rust FFI to libyang
+            println!("cargo:rerun-if-changed=wrapper.h");
+            let bindings = bindgen::Builder::default()
+                .header("wrapper.h")
+                .clang_arg(format!("-I{}/include", cmake_dst.display()))
+                .derive_default(true)
+                .default_enum_style(bindgen::EnumVariation::ModuleConsts)
+                .generate()
+                .expect("Unable to generate libyang3 bindings");
+            bindings
+                .write_to_file(out_file)
+                .expect("Couldn't write libyang3 bindings!");
         }
     }
     #[cfg(not(feature = "bundled"))]
